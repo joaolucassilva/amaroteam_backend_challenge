b79fb070d3a28566a8f01b2d6ac414914af2b7c3
joao lucas <luccas332@gmail.com>
2022-07-18 21:57:40 -0300

    feat: implement jwt

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GetProductsController;
+use App\Http\Controllers\UserRegisterController;
 use App\Http\Controllers\SaveProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register', UserRegisterController::class);
 
-Route::get('/products', GetProductsController::class);
-Route::post('/products', SaveProductController::class);
-//
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::group([
+    'middleware' => 'auth',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::group([
+    'middleware' => 'auth'
+], function () {
+    Route::group([
+        'prefix' => 'products'
+    ], function () {
+        Route::get('/', GetProductsController::class);
+        Route::post('/', SaveProductController::class);
+    });
+});
